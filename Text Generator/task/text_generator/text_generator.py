20659a250f92a075b41a93dbf39e304a92c38f20
Sergii Kamenskyi <serg@apple.com>
2022-04-01 12:30:55 +0300

    PROJECT COMPLETED!

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -118,9 +118,54 @@
 #         start_word = next_word
 #     print(output)
 
-#------------------------------------STAGE 5------------------------------------
+# #------------------------------------STAGE 5------------------------------------
+# from nltk.tokenize import WhitespaceTokenizer
+# from nltk import bigrams
+# from collections import Counter
+# import random
+#
+# PUNCTUATION_MARKS = ['.', '!', '?']
+#
+# file_name = input()
+#
+# tk = WhitespaceTokenizer()
+# with open(file_name, 'r', encoding='utf-8') as reader:
+#     tokens = tk.tokenize(reader.read())
+# bigrams_list = list(bigrams(tokens))
+#
+# bigrams_list_dict = {}
+# for bigram in bigrams_list:
+#     bigrams_list_dict.setdefault(bigram[0], []).append((bigram[1]))
+#
+# for bigram_key in bigrams_list_dict.keys():
+#     bigrams_list_dict[bigram_key] = Counter(bigrams_list_dict[bigram_key])
+#
+# for _i in range(10):
+#     output = ""
+#     start_word = random.choice(list(bigrams_list_dict.keys()))
+#     while not start_word[0].isupper() or start_word[-1] in PUNCTUATION_MARKS:
+#         start_word = random.choice(list(bigrams_list_dict.keys()))
+#     output += start_word
+#     for _j in range(4):
+#         words = [x[0] for x in bigrams_list_dict[start_word].most_common()]
+#         frequency = [x[1] for x in bigrams_list_dict[start_word].most_common()]
+#         next_word = random.choices(words, frequency)[0]
+#         if start_word[-1] in PUNCTUATION_MARKS:
+#             while  not next_word[0].isupper():
+#                 next_word = random.choices(words, frequency)[0]
+#         output = output + " " + next_word
+#         start_word = next_word
+#     while start_word[-1] not in PUNCTUATION_MARKS:
+#         words = [x[0] for x in bigrams_list_dict[start_word].most_common()]
+#         frequency = [x[1] for x in bigrams_list_dict[start_word].most_common()]
+#         next_word = random.choices(words, frequency)[0]
+#         output = output + " " + next_word
+#         start_word = next_word
+#     print(output)
+
+#------------------------------------STAGE 6------------------------------------
 from nltk.tokenize import WhitespaceTokenizer
-from nltk import bigrams
+from nltk import trigrams
 from collections import Counter
 import random
 
@@ -131,7 +176,11 @@ file_name = input()
 tk = WhitespaceTokenizer()
 with open(file_name, 'r', encoding='utf-8') as reader:
     tokens = tk.tokenize(reader.read())
-bigrams_list = list(bigrams(tokens))
+trigrams_list = list(trigrams(tokens))
+# print(trigrams_list[:10])
+
+bigrams_list = [[x[0] + " " + x[1], x[2]] for x in trigrams_list]
+# print(bigrams_list[:10])
 
 bigrams_list_dict = {}
 for bigram in bigrams_list:
@@ -143,22 +192,22 @@ for bigram_key in bigrams_list_dict.keys():
 for _i in range(10):
     output = ""
     start_word = random.choice(list(bigrams_list_dict.keys()))
-    while not start_word[0].isupper() or start_word[-1] in PUNCTUATION_MARKS:
+    while not start_word[0].isupper() or start_word.split()[0][-1] in PUNCTUATION_MARKS:
         start_word = random.choice(list(bigrams_list_dict.keys()))
     output += start_word
-    for _j in range(4):
+    for _j in range(3):
         words = [x[0] for x in bigrams_list_dict[start_word].most_common()]
         frequency = [x[1] for x in bigrams_list_dict[start_word].most_common()]
         next_word = random.choices(words, frequency)[0]
         if start_word[-1] in PUNCTUATION_MARKS:
-            while  not next_word[0].isupper():
+            while not next_word[0].isupper():
                 next_word = random.choices(words, frequency)[0]
         output = output + " " + next_word
-        start_word = next_word
+        start_word = start_word.split()[1] + " " + next_word
     while start_word[-1] not in PUNCTUATION_MARKS:
         words = [x[0] for x in bigrams_list_dict[start_word].most_common()]
         frequency = [x[1] for x in bigrams_list_dict[start_word].most_common()]
         next_word = random.choices(words, frequency)[0]
         output = output + " " + next_word
-        start_word = next_word
+        start_word = start_word.split()[1] + " " + next_word
     print(output)
